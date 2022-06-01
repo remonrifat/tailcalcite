11bb24507e4ce614a76bedb03aac89c885fb39e4
unknown <remonrifat2000@gmail.com>
2022-06-01 20:57:50 +0600

    Updates

---
+++ precache-manifest.c9ec2fd53fb13f96652357c94a74f7c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae55d2a4ed4a095a9ca84633c935a81",
+    "url": "/tailcalcite/index.html"
+  },
+  {
+    "revision": "2d606aa79a0ed045ce34",
+    "url": "/tailcalcite/static/css/main.db7ec9a3.chunk.css"
+  },
+  {
+    "revision": "4e40af6ecd8a3ab7826f",
+    "url": "/tailcalcite/static/js/2.faa93b64.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tailcalcite/static/js/2.faa93b64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d606aa79a0ed045ce34",
+    "url": "/tailcalcite/static/js/main.717e3c88.chunk.js"
+  },
+  {
+    "revision": "b4fb01638e273e66a688",
+    "url": "/tailcalcite/static/js/runtime-main.12b6824a.js"
+  }
+]);
